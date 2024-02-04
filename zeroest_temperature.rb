9773a4025d3e3484d436121267b2fe5c30bd8cab
Seamoon Pandey <hackedasocial@gmail.com>
2024-02-04 23:36:26 +0545

    add zeroest temperature

---
+++ zeroest_temperature.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+gets.to_i
+
+# Convert inputs to integers and store in array
+inputs = gets.split.map(&:to_i)
+
+# Sort the array of temperatures by absolute value, prioritizing positive values
+sorted_temperatures = inputs.sort_by { |t| [t.abs, -t] }
+
+result = sorted_temperatures.first
+
+puts result.nil? ? 0 : result
